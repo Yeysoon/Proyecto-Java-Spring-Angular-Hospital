83e9a77eb168086e7c6299a9d7155294d8394386
Yeysoon <106795715+Yeysoon@users.noreply.github.com>
2024-02-26 21:16:06 -0600

    Add files via upload

---
+++ demo-proyecto-progra/src/main/java/com/umg/proyecto/demoproyectoprogra/DemoProyectoPrograApplication.java
@@ -0,0 +1,13 @@
+package com.umg.proyecto.demoproyectoprogra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProyectoPrograApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProyectoPrograApplication.class, args);
+	}
+
+}
---
+++ demo-proyecto-progra/src/test/java/com/umg/proyecto/demoproyectoprogra/DemoProyectoPrograApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.proyecto.demoproyectoprogra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProyectoPrograApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
