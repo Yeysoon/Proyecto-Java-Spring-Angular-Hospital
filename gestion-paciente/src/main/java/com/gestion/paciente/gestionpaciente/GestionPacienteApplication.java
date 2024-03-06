e43392e9436c479c967211061f43efec82cd881c
Yeysoon <106795715+Yeysoon@users.noreply.github.com>
2024-03-05 21:51:07 -0600

    Subir el proyecto

---
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/GestionPacienteApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.paciente.gestionpaciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPacienteApplication.class, args);
+	}
+
+}
---
+++ gestion-paciente/src/test/java/com/gestion/paciente/gestionpaciente/GestionPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.paciente.gestionpaciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
