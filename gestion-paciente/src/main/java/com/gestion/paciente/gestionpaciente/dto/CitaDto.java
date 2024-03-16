e14479ae10d2e2d9980b174c446e03ad78ebfa96
jonathan <jdelacruzr7@umg.edu.gt>
2024-03-15 20:37:00 -0600

    avance de la mañana

---
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/dto/CitaDto.java
@@ -0,0 +1,18 @@
+package com.gestion.paciente.gestionpaciente.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CitaDto {
+
+    private LocalDate fechaCita;
+    private int horaCita;
+    private String motivoCita;
+
+
+
+}
