6727477c5d589bad93335bd9aaa22edb9f745dd1
yeysoon <yeysonaldair13@gmail.com>
2024-03-15 22:07:02 -0600

    Simplicacion de Codigo

--- gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/controllers/CitasMedicasController.java
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/controllers/CitasMedicasController.java
@@ -3,11 +3,7 @@ package com.gestion.paciente.gestionpaciente.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gestion.paciente.gestionpaciente.dto.CitaDto;
 import com.gestion.paciente.gestionpaciente.entidades.Citas;
