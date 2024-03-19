c6c15e470351bec46b7e4676515f862671d7ebd4
yeysoon <yeysonaldair13@gmail.com>
2024-03-18 21:19:14 -0600

    Eliminacion de Clases de momento no necesarias

--- gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/repositorio/TipoEnfermedadesRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.paciente.gestionpaciente.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gestion.paciente.gestionpaciente.entidades.TiposEnfermedades;
-
-public interface TipoEnfermedadesRepositorio extends CrudRepository <TiposEnfermedades, Long> {
-
-}
