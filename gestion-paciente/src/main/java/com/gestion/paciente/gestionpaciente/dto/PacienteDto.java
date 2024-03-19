92d4d13d06b2004e4890410556d968dfad0794d2
yeysoon <yeysonaldair13@gmail.com>
2024-03-18 21:29:26 -0600

    Error en varible

--- gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/dto/PacienteDto.java
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/dto/PacienteDto.java
@@ -12,7 +12,7 @@ public class PacienteDto {
     private Long idPacientes;
     private String nombrePaciente;
     private Integer edad;
-    private LocalDate fecha;
+    private LocalDate fechaNacimiento;
     private String genero;
     private String direccion;
     private Integer telefono;
