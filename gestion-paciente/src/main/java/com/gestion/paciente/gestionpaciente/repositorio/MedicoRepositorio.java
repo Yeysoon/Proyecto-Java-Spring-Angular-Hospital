2e70fab68ebd73f3237f82939776d95714d24630
RicardoCruzLemus21 <rcruzl5@miumg.edu.gt>
2024-03-19 22:00:38 -0600

    Creacion de interface MedicoRepositorio

---
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/repositorio/MedicoRepositorio.java
@@ -0,0 +1,9 @@
+package com.gestion.paciente.gestionpaciente.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gestion.paciente.gestionpaciente.entidades.Medicos;
+
+public interface MedicoRepositorio extends CrudRepository <Medicos, Long>{
+
+}
