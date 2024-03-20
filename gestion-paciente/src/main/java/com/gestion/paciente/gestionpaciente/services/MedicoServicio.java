daecbfa54732009b3f1d0a0fafba1eced5a03143
RicardoCruzLemus21 <rcruzl5@miumg.edu.gt>
2024-03-19 22:10:08 -0600

    Servicio de medico creado

---
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/services/MedicoServicio.java
@@ -0,0 +1,27 @@
+package com.gestion.paciente.gestionpaciente.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+
+import com.gestion.paciente.gestionpaciente.entidades.Medicos;
+import com.gestion.paciente.gestionpaciente.repositorio.MedicoRepositorio;
+
+@Service
+public class MedicoServicio {
+    @Autowired
+    private MedicoRepositorio medicoRepositorio;
+
+    public List<Medicos> consultarMedico(){
+        return (List<Medicos>) medicoRepositorio.findAll();
+    }
+
+    @SuppressWarnings("null")
+    public Medicos registrarMedico(Medicos medicos){
+        return medicoRepositorio.save(medicos);
+    }
+
+    
+
+}
