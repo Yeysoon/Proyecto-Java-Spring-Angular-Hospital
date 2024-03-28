21ede72f48fb0c18fc8d316b337356b113034cde
yeysoon <yeysonaldair13@gmail.com>
2024-03-28 08:14:42 -0600

    Implementacion  Update, Delete

--- gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/controllers/MedicoController.java
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/controllers/MedicoController.java
@@ -3,11 +3,16 @@ package com.gestion.paciente.gestionpaciente.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestion.paciente.gestionpaciente.dto.MedicoDto;
@@ -29,7 +34,6 @@ public class MedicoController {
         return servicioMedico.consultarMedico();
     }
 
-
     @PostMapping ("/registroMedicos")
     public Medicos registrarMedico(@RequestBody MedicoDto medicoJson){
         Medicos medicos = new Medicos();
@@ -45,10 +49,35 @@ public class MedicoController {
 
     }
 
-    /* 
     @GetMapping ("/medicos/{id}")
-    public Medicos buscarPorId(){
-
+    public Medicos buscarPorId(@PathVariable Long id){
+        return servicioMedico.findById(id);
     }
-*/
+
+    @PostMapping ("/guardarMedicos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Medicos guardarMedicos(@RequestBody Medicos medicos){
+        return servicioMedico.save(medicos);
+    } 
+
+    @PutMapping ("/medicos/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Medicos actualizarMedicos(@RequestBody Medicos medicos, @PathVariable Long id){
+        Medicos medicoActual = servicioMedico.findById(id);
+
+        medicoActual.setNombreMedico(medicos.getNombreMedico());
+        medicoActual.setEspecialidad(medicos.getEspecialidad());
+        medicoActual.setLicenciaMedica(medicos.getLicenciaMedica());
+        medicoActual.setCargo(medicos.getCargo());
+        medicoActual.setTurno(medicos.getTurno());
+
+        return servicioMedico.save(medicoActual);
+    }
+
+    @DeleteMapping ("/medicos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminarMedicos(@PathVariable Long id){
+        servicioMedico.delete(id);
+    }
+
 }
