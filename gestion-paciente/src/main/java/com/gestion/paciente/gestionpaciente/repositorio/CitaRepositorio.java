ff5471e5aa99926b64513e5e4bd0dc826b8f4d49
yeysoon <yeysonaldair13@gmail.com>
2024-04-09 22:27:52 -0600

    Eliminacion de API citas Medicas

--- gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/repositorio/CitaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.paciente.gestionpaciente.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gestion.paciente.gestionpaciente.entidades.Citas;
-
-public interface CitaRepositorio extends CrudRepository <Citas, Long> {
-
-}
