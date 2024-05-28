c0f5146578e224b8592c450484d40f3684108c14
yeysoon <yeysonaldair13@gmail.com>
2024-05-28 17:17:56 -0600

    Ajustes pequeños

--- gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/controllers/DoctoresController.java
+++ gestion-paciente/src/main/java/com/gestion/paciente/gestionpaciente/controllers/DoctoresController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +21,10 @@ import com.gestion.paciente.gestionpaciente.dto.DoctoresDto;
 import com.gestion.paciente.gestionpaciente.entidades.Doctores;
 import com.gestion.paciente.gestionpaciente.services.DoctoresServicio;
 
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
-@RequestMapping("/Doctores")
+@RequestMapping("/api")
 public class DoctoresController {
 
      @Autowired
@@ -32,7 +35,7 @@ public class DoctoresController {
      }
 
 
-     @GetMapping
+     @GetMapping("/doctores")
      public ResponseEntity<List<Doctores>> getList(){
         return ResponseEntity.ok(doctoresServicio.getAll());
      }
